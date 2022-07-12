dbf20e3c7879c8ef613ceaea14f468d9efb4cea7
DanieleKun <pucciodaniele95@gmail.com>
2022-07-12 15:52:03 +0200

    add dinamic comics list

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,25 @@
+<header>
+    <header>
+        <a href="">
+          <img src="{{asset('img/dc-logo.png')}}" alt="Logo DC">
+        </a>
+        <nav>
+          <ul>
+            <li><a href="#">Ciao</a></li>
+            <li>
+                <a href="#">Characters</a>
+                <a href="#">Comics</a>
+                <a href="#">Movies</a>
+                <a href="#">Tv</a>
+                <a href="#">Games</a>
+                <a href="#">Collectibles</a>
+                <a href="#">Videos</a>
+                <a href="#">Fans</a>
+                <a href="#">News</a>
+                <a href="#">Shop</a>
+            </li>
+          </ul>
+        </nav>
+    
+      </header>
+</header>
